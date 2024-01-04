8251d333cbcbd08147a97952ec4ffcaf88d823c6
claranceJ <77950647+claranceJ@users.noreply.github.com>
2024-01-04 05:28:59 +0000

    working with dictionaries and identity operators

---
+++ sets.py
@@ -0,0 +1,41 @@
+numbers = [
+    2,
+    6,
+    8,
+    9,
+    8,
+    2,
+    9,
+    6,
+    10,
+    25,
+    3,
+    89,
+    34,
+    67,
+    10,
+    22,
+    11,
+    2,
+    6,
+    8,
+    9,
+    8,
+    2,
+    9,
+    6,
+    10,
+    25,
+    3,
+]
+
+unique_num = set(numbers)
+print(len(unique_num))
+print(unique_num)
+
+fruit = {"apple", "banana", "orange", "grapefruit"}
+print("watermelon" in fruit)
+fruit.add("sugar")
+print(fruit)
+fruit.pop()
+print(fruit)
