a249e6b33b37f1620b7f8fc977e3ea2640e37c09
claranceJ <77950647+claranceJ@users.noreply.github.com>
2024-01-04 04:32:44 +0000

    Tuple unpacking

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 pointers = 4, 7, 9
+
+#tuple unpacking
 length, width, height = pointers
 
+print("The 3 pointers are {}X{}X{}" .format(length, width, height))
+
+#OR
+length, width, height = 4, 7, 9
 print("The 3 pointers are {}X{}X{}" .format(length, width, height))
